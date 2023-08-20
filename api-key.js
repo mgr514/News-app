da1d0e04fe0ebeaf03f49e20a9f894503d4c12b1
mgr514 <mgr514@mun.ca>
2023-08-19 21:56:12 -0230

    notes added

--- api-key.js
+++ api-key.js
@@ -1,1 +1,7 @@
 let apiKey = "bdb56512aa7b42528ef41d665e007c3f";
+
+//api source https://newsapi.org/docs/endpoints/sources
+
+//why do the tabs not come up like the tutorial?
+
+//How do I fix the 404 error message?
